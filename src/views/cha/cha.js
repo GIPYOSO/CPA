c1e8fa498f2ce07f913f9f676f2427f13b4c630d
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2022-10-24 15:00:40 +0900

    "api 수정"

--- src/views/cha/cha.js
+++ src/views/cha/cha.js
@@ -63,8 +63,8 @@ window.onload = async function () {
         browser = 'Unknown';
     }
     const datas = { ip, referrer, browser, os }
-    // const postData = await Api.post('http://49.247.45.148:80/api/visitor', datas);
-    const postData = await Api.post('http://localhost:80/api/visitor', datas);
+    const postData = await Api.post('http://49.247.45.148:80/api/visitor', datas);
+    // const postData = await Api.post('http://localhost:80/api/visitor', datas);
     // window.localStorage.setItem('visitor', "on")
     // }
 }
